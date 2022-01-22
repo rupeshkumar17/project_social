ba83387607296b488a5afc2bf952e9457080daa6
rupesh <rupesh.kumar1733@gmail.com>
2022-01-22 21:50:41 +0530

    users and posts

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const fs = require("fs");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 dotenv.config();
@@ -20,16 +22,27 @@ const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
+//apiDocs
+app.get("/", (req, res) => {
+  fs.readFile("docs/apiDocs.json", (err, data) => {
+    if (err) {
+      res.status(400).json({ error: err });
+    }
+    const docs = JSON.parse(data);
+    res.json(docs);
+  });
+});
+
 const myownMiddleware = (req, res, next) => {
   console.log("middleware is applied");
   next();
 };
 //middleware
 app.use(morgan("dev"));
-// app.use(myownMiddleware)
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 app.use("/", userRoutes);
