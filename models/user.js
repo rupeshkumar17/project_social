a7a2461116ef75c320baea4410c2344402ef6303
rupesh <rupesh.kumar1733@gmail.com>
2022-01-27 19:44:08 +0530

    follow and unfollow

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
 const crypto = require('crypto');
+const {ObjectId}=mongoose.Schema
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,8 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  following: [{ type: ObjectId, ref: 'User' }],
+  followers: [{ type: ObjectId, ref: 'User' }],
 });
 
 //virtual field
--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,24 @@ const {
   updateUser,
   deleteUser,
   userPhoto,
+  addFollowing,
+  addFollower,
+  removeFollowing,
+  removeFollower,
 } = require('../controllers/user');
-const express = require('express');
+const express = require('express'); 
 const { requireSignin } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/users', allUsers); 
+router.put('/user/follow', requireSignin, addFollowing, addFollower);
+router.put('/user/follow', requireSignin, removeFollowing, removeFollower);
+
+router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
 router.put('/user/:userId', requireSignin, updateUser);
 router.delete('/user/:userId', requireSignin, deleteUser);
-router.get('/user/photo/:userId',userPhoto)
+router.get('/user/photo/:userId', userPhoto);
 
 //any route containing user id aur app will excute user id
 router.param('userId', userById);
