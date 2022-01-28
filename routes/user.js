623cdda34f41ed1a34c4dfe451bf6c2a81110232
rupesh <rupesh.kumar1733@gmail.com>
2022-01-28 11:18:11 +0530

    users follow and unfollow complete

--- routes/user.js
+++ routes/user.js
@@ -9,21 +9,24 @@ const {
   addFollower,
   removeFollowing,
   removeFollower,
+  findPeople,
 } = require('../controllers/user');
-const express = require('express'); 
+const express = require('express');
 const { requireSignin } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.put('/user/follow', requireSignin, addFollowing, addFollower);
-router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower);
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower); //error follow instead of unfollow
 
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
 router.put('/user/:userId', requireSignin, updateUser);
 router.delete('/user/:userId', requireSignin, deleteUser);
+//photo
 router.get('/user/photo/:userId', userPhoto);
-
+//who to follow
+router.get('/user/findpeople/:userId', requireSignin, findPeople);
 //any route containing user id aur app will excute user id
 router.param('userId', userById);
 
